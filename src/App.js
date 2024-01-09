a4f837c5a4d5b6c5b8fd9a91d5c0033eddb2060a
Ryan Chang <rchang05@usc.edu>
2024-01-08 20:24:07 -0800

    Minor fixes to deployment

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
 
   return (
     <>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <ScrollToTop>
           <Routes>
             <Route path="/" element={<Home />} />
